453b7ef7fc0b1735dad9dcc0c0b8773aaf326491
Justin Stone <justinstone@Justins-MacBook-Pro-2.local>
2022-06-02 18:59:12 -0400

    Set up html document for each section, started selecting the classes in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+var cityFormEl = document.querySelector('#city-form')
+var cityNameEl = document.querySelector('#city');
+var weatherEl = document.querySelector('#weather');
+var weatherContainerEl = document.querySelector('#weather')
+
+
 function init() {
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=Nashville&limit=1&appid=79fd1882fd1e16cad8be5bc759899879')
     .then(function(response) {
@@ -9,15 +15,34 @@ function init() {
     });
 }
 
-
 function getOneCallWeather(lat, lon){
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=79fd1882fd1e16cad8be5bc759899879`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=79fd1882fd1e16cad8be5bc759899879&units=imperial`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var temp = data.current.temp;
+            var wind_speed = data.current.wind_speed;
+            var uvi = data.current.uvi;
+            var humidity = data.current.humidity;
+            var tempEl = document.createElement('p');
+            var windEl = document.createElement('p');
+            var uvEl = document.createElement('p');
+            var humidityEl = document.createElement('p');
 
-}
+            tempEl.textContent = "Temperature: " + temp;
+            windEl.textContent = "Wind Speed: " + wind_speed;
+            uvEl.textContent = "UVI: " + uvi;
+            humidityEl.textContent = "Humidity: " + humidity;
+
+            document.body.append(tempEl, windEl, uvEl, humidityEl);
+
+            for(var i = 0; i < data.daily.length; i++) {
+                var day = data.daily[i];
+                console.log(day);
+            }
+        });
+
+ }
+
 init();
